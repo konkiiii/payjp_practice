6e0232cd7f7c22f2fad05d27a8c2bfcbc6c0a47c
konkiiii <kintaro0827@icloud.com>
2023-02-15 22:59:10 +0900

    決済機能_クライアントサイド実装_3
    PAY.JPのテスト公開鍵を取得し設定する

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,4 +1,6 @@
 const pay = () => {
+  const payjp = Payjp('pk_test_***********************')// PAY.JPテスト公開鍵
+  // 後で環境変数に設定する
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
